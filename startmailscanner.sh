7fa24b2e53d6da70b37fce384e8fa925d5d76c16
Clustertech sysadmin <sysadmin@clustertech.com>
2020-01-09 11:11:00 +0800

    initial commit

---
+++ startmailscanner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+trap "postfix stop" SIGTERM
+rsyslogd
+newaliases
+postfix check
+/usr/lib/MailScanner/init/ms-init start
+/usr/lib/MailScanner/init/msmilter-init start
+/usr/libexec/postfix/master -d &
+wait
